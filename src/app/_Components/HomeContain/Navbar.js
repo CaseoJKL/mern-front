fbdc51ee633fb8208b7640a17416f7f19f7580cf
M4soudalizadeh <M4soudalizadeh@gmail.com>
2023-08-09 15:00:16 +0330

    Login page => add image in left side section => working ont it...

--- src/app/_Components/HomeContain/Navbar.js
+++ src/app/_Components/HomeContain/Navbar.js
@@ -9,33 +9,41 @@ import {
 import Image from "next/image";
 const Navbar = () => {
   return (
-    <header className="home_header">
-      <nav>
-        <section>
+    <header className="z-20 text-white bg-zinc-800 border-b-[1px] border-zinc-600 fixed top-0 left-0 md:left-[280px] w-full md:w-[calc(100%-280px)]">
+      <nav className="flex items-center justify-between h-20 px-4">
+        <section className="flex items-center justify-center">
           <FontAwesomeIcon className="mr-2" icon={faHome} />
-          <h1>Home</h1>
+          <h1 className="hidden sm:block sm:text-xl">Home</h1>
         </section>
-        <ul>
-          <li className="activate">
-            <h4>Explore</h4>
+        <ul className="flex justify-around items-center px-1 ml-1 bg-zinc-700 rounded w-[140px] h-[30px] sm:h-[50px] sm:w-[450px]">
+          <li className="px-1 py-1 text-center rounded cursor-pointer flex-row-g sm:py-3 sm:px-6">
+            <h4 className="text-[5px] sm:text-[12px] md:text-[14px]">
+              Explore
+            </h4>
           </li>
-          <li>
-            <h4>Community feed</h4>
+          <li className="px-1 py-1 text-center rounded cursor-pointer flex-row-g sm:py-3 sm:px-6">
+            <h4 className="text-[5px] sm:text-[12px] md:text-[14px]">
+              Community feed
+            </h4>
           </li>
-          <li>
-            <h4>Mutual friend</h4>
-            <span>12</span>
+          <li className="px-1 py-1 text-center rounded cursor-pointer activate flex-row-g sm:py-3 sm:px-6">
+            <h4 className="text-[5px] sm:text-[12px] md:text-[14px]">
+              Mutual friend
+            </h4>
+            <span className="text-[5px] bg-zinc-800 rounded-full px-2 sm:py-1 ml-2 sm:text-xs">
+              12
+            </span>
           </li>
         </ul>
-        <section>
-          <div>
-            <FontAwesomeIcon className="mr-2" icon={faComment} />
+        <section className="flex items-center justify-center">
+          <div className="w-[20px] h-[20px] sm:w-[35px] sm:h-[35px] bg-zinc-700 rounded-full mx-1 flex-row-g cursor-pointer">
+            <FontAwesomeIcon icon={faComment} />
           </div>
-          <div>
-            <FontAwesomeIcon className="mr-2" icon={faBell} />
+          <div className="w-[20px] h-[20px] sm:w-[35px] sm:h-[35px] bg-zinc-700 rounded-full mx-1 flex-row-g cursor-pointer">
+            <FontAwesomeIcon icon={faBell} />
           </div>
-          <h2>Sara N</h2>
-          <div className="relative">
+          <h2 className="hidden sm:block sm:text-center">Sara N</h2>
+          <div className="relative w-[20px] h-[20px] sm:w-[35px] sm:h-[35px] bg-zinc-700 rounded-full mx-1 flex-row-g cursor-pointer">
             <Image className="rounded-full" src={profile} alt="" />
             <FontAwesomeIcon
               className="mr-2 absolute px-[1px] py-[1px] top-3 lg:top-6 -right-1 text-xs lg:px-[2px] lg:py-[2px] cursor-pointer bg-white text-black rounded-full"
--- src/app/_Components/HomeContain/Trend.js
+++ src/app/_Components/HomeContain/Trend.js
@@ -3,40 +3,40 @@ import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 const Trend = () => {
   return (
     <section className="trend !w-[300px] hidden lg:block mb-20">
-      <header className=" flex justify-between items-center">
+      <header className="flex items-center justify-between ">
         <h2>Today Trending</h2>
         <FontAwesomeIcon icon={faAngleDown} />
       </header>
       <ul>
-        <li className="my-6 flex justify-between items-center">
+        <li className="flex items-center justify-between my-6">
           <div>
             <h3 className="text-white">Figma maintenance</h3>
-            <p className="text-sm mt-1">125 post today</p>
+            <p className="mt-1 text-sm">125 post today</p>
           </div>
           <span className="text-xs bg-zinc-700 px-1 py-[2px] rounded-full ">
             66 in 1 hour
           </span>
         </li>
-        <li className="my-6 flex justify-between items-center">
+        <li className="flex items-center justify-between my-6">
           <div>
             <h3 className="text-white">Blender Update</h3>
-            <p className="text-sm mt-1">117 post today</p>
+            <p className="mt-1 text-sm">117 post today</p>
           </div>
           <span className="text-xs bg-zinc-700 px-1 py-[2px] rounded-full ">
             45 in 1 hour
           </span>
         </li>
-        <li className="my-6 flex justify-between items-center">
+        <li className="flex items-center justify-between my-6">
           <div>
             <h3 className="text-white">Javascript new</h3>
-            <p className="text-sm mt-1">32 post today</p>
+            <p className="mt-1 text-sm">32 post today</p>
           </div>
           <span className="text-xs bg-zinc-700 px-1 py-[2px] rounded-full ">
             24 in 1 hour
           </span>
         </li>
       </ul>
-      <footer className="my-3 py-2 text-blue-600 text-center cursor-pointer">
+      <footer className="py-2 my-3 text-center text-blue-600 cursor-pointer">
         See all
       </footer>
     </section>
